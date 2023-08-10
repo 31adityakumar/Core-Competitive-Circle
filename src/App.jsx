3a9df2d7e077616461842ca3862715dcae008921
bipangshu <bumba8812@gmail.com>
2023-08-10 15:28:20 +0530

    Routes Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import Home from './Pages/Home/Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Team from "./Pages/Team/Team";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
 function App() {
-
   return (
     <>
-      <Home/>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} key="route-home-screen" />
+          <Route exact path="/team" element={<Team />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
