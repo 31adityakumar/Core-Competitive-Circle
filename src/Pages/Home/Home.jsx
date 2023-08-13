7463d8263659991ed031ea88ee35a4c6b49f2e9c
Yuv0101 <yuvraj21_ug@ece.nits.ac.in>
2023-08-13 23:05:05 +0530

    UPD: Departments

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Department from '../../Components/Department/Department';
 import Hero from '../../Components/Hero/Hero';
 import './Home.module.scss'
 const Home = () => {
     return(
         <>
         <Hero/>
+        <Department/>
         </>
     )
 }
