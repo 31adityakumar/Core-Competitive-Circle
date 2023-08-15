c1cec106e8993356ff65bdf01201f403f6023477
bipangshu <bumba8812@gmail.com>
2023-08-16 00:20:47 +0530

    ADD: Pillars

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from '../../Components/Hero/Hero';
+import Pillars from '../../Components/Pillars/Pillars'
 import './Home.module.scss'
 const Home = () => {
     return(
         <>
         <Hero/>
+        <Pillars />
         </>
     )
 }
