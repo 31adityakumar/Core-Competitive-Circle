2cb3bdf7d2b2b138c7a429066df7787973a86d49
Aditya <kumar.31aditya@gmail.com>
2023-08-17 08:25:52 +0530

    FIX: Build Error

--- src/Components/Pillars/Pillars.jsx
+++ src/Components/Pillars/Pillars.jsx
@@ -1,18 +1,17 @@
-import {useRef} from "react";
+import { useRef } from "react";
 import ItemCard from "./ItemCard";
 import styles from "./Pillars.module.scss";
 import Dilip from "/images/Dilip.png";
 
 const Pillars = () => {
   let cardHolder = useRef();
-  
 
-  function handleNext () {
-    cardHolder.current.scrollLeft += (cardHolder.current.offsetWidth);
+  function handleNext() {
+    cardHolder.current.scrollLeft += cardHolder.current.offsetWidth;
   }
 
-  function handlePrev () {
-    cardHolder.current.scrollLeft -= (cardHolder.current.offsetWidth);
+  function handlePrev() {
+    cardHolder.current.scrollLeft -= cardHolder.current.offsetWidth;
   }
 
   let info = [
@@ -21,21 +20,21 @@ const Pillars = () => {
       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur neque id laboriosam. Neque ipsum, quasi iusto minima est optio beatae ducimus nobis suscipit aspernatur qui eius in debitis nisi aliquam.",
       name: "Prof. Dilip Kumar Baidya",
       designation: "Director, NIT Sichar",
-      img : Dilip
+      img: Dilip,
     },
     {
       id: "2",
       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur neque id laboriosam. Neque ipsum, quasi iusto minima est optio beatae ducimus nobis suscipit aspernatur qui eius in debitis nisi aliquam.",
       name: "Prof. Dilip Kumar Baidya",
       designation: "Director, NIT Sichar",
-      img : Dilip
+      img: Dilip,
     },
     {
       id: "3",
       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur neque id laboriosam. Neque ipsum, quasi iusto minima est optio beatae ducimus nobis suscipit aspernatur qui eius in debitis nisi aliquam.",
       name: "Prof. Dilip Kumar Baidya",
       designation: "Director, NIT Sichar",
-      img : Dilip
+      img: Dilip,
     },
   ];
 
@@ -46,18 +45,18 @@ const Pillars = () => {
       </div>
 
       <div className={styles.Card_holder} ref={cardHolder}>
-          {info.map((element) => {
-            return (
-              <ItemCard
-                key={element.id}
-                id={element.id}
-                desc={element.desc}
-                name={element.name}
-                designation={element.designation}
-                img={element.img}
-              />
-            )
-          })}
+        {info.map((element) => {
+          return (
+            <ItemCard
+              key={element.id}
+              id={element.id}
+              desc={element.desc}
+              name={element.name}
+              designation={element.designation}
+              img={element.img}
+            />
+          );
+        })}
       </div>
       <div className={`${styles.btns} ${styles.btns0}`}>
         <button className={styles.prev} onClick={handlePrev}>
